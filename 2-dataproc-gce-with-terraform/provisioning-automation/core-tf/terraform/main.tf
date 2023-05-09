23b9e742ee378371a2e998da25a43fc332f18cae
Anagha Khanolkar <76123576+anagha-google@users.noreply.github.com>
2023-05-09 16:43:05 -0500

    Update main.tf
    To ensure Dataproc job logs are sent to Cloud Logging

--- 2-dataproc-gce-with-terraform/provisioning-automation/core-tf/terraform/main.tf
+++ 2-dataproc-gce-with-terraform/provisioning-automation/core-tf/terraform/main.tf
@@ -421,7 +421,12 @@ resource "google_dataproc_cluster" "sphs_creation" {
         "mapred:mapreduce.jobhistory.done-dir"="${local.dpgce_spark_sphs_bucket_fqn}/events/mapreduce-job-history/done"
         "mapred:mapreduce.jobhistory.intermediate-done-dir"="${local.dpgce_spark_sphs_bucket_fqn}/events/mapreduce-job-history/intermediate-done"
         "yarn:yarn.nodemanager.remote-app-log-dir"="${local.dpgce_spark_sphs_bucket_fqn}/yarn-logs"
-
+        "dataproc:dataproc.logging.stackdriver.enable"=true
+        "dataproc:dataproc.monitoring.stackdriver.enable"=true
+        "yarn:yarn.log-aggregation.enabled"=true
+        "dataproc:dataproc.logging.stackdriver.job.yarn.container.enable"=true
+        "dataproc:jobs.file-backed-output.enable"=true
+        "dataproc:dataproc.logging.stackdriver.job.driver.enable"=true
       }      
     }
     gce_cluster_config {
